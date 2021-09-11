42c651d4bfc074df74e904a566a27d918c659d9a
diffran15 <diffran5@gmail.com>
2021-09-11 14:27:04 +0700

    Incremental-Game

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+
+    public void OnPointerDown(PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+
+    }
+
+}
